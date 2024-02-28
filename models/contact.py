20757d83551f11502b374d2a70f869e823d70af8
Jose Daniel Anacona <jdanaconar@gmail.com>
2024-02-28 10:07:30 -0500

    Creando base para el proyecto N1 en py

---
+++ models/contact.py
@@ -0,0 +1,12 @@
+from utils.db import db 
+
+class Contact(db.Model):
+    id = db.Colum(db.Integer, primary_key=True)
+    fullname = db.Colum(db.String(100))
+    email = db.Colum(db.String(100))
+    phone = db.Colum(db.String(100))
+
+    def __init__(self, fullname, email, phone):
+        self.fullname = fullname
+        self.email = email
+        self.phone = phone
